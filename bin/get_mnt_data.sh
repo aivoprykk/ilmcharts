9dbd7d01b491afcca354e8c75d7ac20c57de19df
Aivo Prukk <aivo@majasa.ee>
2014-08-13 14:37:42 +0300

    Data parser for balticroads.net

---
+++ bin/get_mnt_data.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+PATH=$PATH:/usr/local/bin
+lab=0
+force=0
+test=0
+dry=""
+last=""
+dir=`dirname $0`
+echo "$dir" | grep -q '^/' || dir=`pwd`/$dir
+path=`echo "$dir"|sed -e 's/\/\?bin//'`
+[ x"$path" = x"" ] && path="." || path=$path
+[ -d $path ] || { echo $path not found; exit; }
+while [ $# -ge 1 ]; do
+        case "$1" in
+                *force)
+                	force=1
+                	lab=1
+                	shift
+                	;;
+                *lab)
+                    lab=1
+                    shift
+                    ;;
+                *test)
+                    test=1
+                    shift
+                    ;;
+                *dry)
+                    dry="1"
+                    shift
+                    ;;
+        esac
+        shift
+done
+(
+cd $path
+
+date=`date '+%F'`
+min=`date '+%H:%M'`;
+
+i=0
+cat $path/places.conf|grep -v '^#'|while read j; do
+#for j in "${places[@]}"; do
+
+IFS=:
+set $j
+place=$1
+title=$2
+wg=$3
+yr=$4
+emhi=$5
+mnt=$6
+IFS=' '
+
+(
+cd public
+#echo $place":"$title;
+[ $test -eq 0 ] || continue;
+[ x"$place" = x"" ] && continue;
+if [ x"$mnt" != x"" ]; then
+mnt_url='http://www.balticroads.net/?getstationdata=1&mapstation='$mnt
+mnt_out="mnt_data/"$place
+mnt_temp="arc-file-$place.html"
+mnt_file="ARC-"$date".txt"
+
+[ -d "$mnt_out" ] || mkdir -p "$mnt_out"
+
+if [ -f $mnt_out/$mnt_file ]; then
+last_stamp=`cat $mnt_out/$mnt_file|tail -1|awk '{ sub("([0-9][0-9][0-9][0-9])","&-",$1);sub("-([0-9][0-9])","&-"); print " " $1 " " $2 }'`
+last_min=`echo $last_stamp|awk '{sub("[0-9][0-9]:","",$2);sub("^0","",$2);print $2}';`
+last_hour=`echo $last_stamp|awk '{sub(":[0-9][0-9]","",$2);sub("^0","",$2);print $2}';`
+cur_min=`echo $min|sed -e 's/.*://;s/^0//'`
+cur_hour=`echo $min|sed -e 's/:.*//;s/^0//'`
+let 'h=cur_hour-last_hour';
+let 'm=cur_min-last_min';
+[ $h -lt 0 ] && {
+ let 'h=cur_hour+24-last_hour'
+}
+[ $h -eq 0 -o $h -eq 1 ] && {
+  [ $m -lt 0 ] && {
+    let 'm=cur_min+60-last_min'
+  }
+  [ $h -ne 1 -a $m -ge 0 -a $m -lt 11 ] && {
+    if [ $lab -gt 0 ]; then
+      echo "minuteid vahem kui 11: $min > $last_stamp";
+    fi
+    [ $force -eq 0 ] && exit;
+  }
+  [ $h -eq 0 -a $m -eq 11 ] && {
+    s=`date '+%S'`
+    w=8
+    if [ $s -lt $w ]; then
+      let 's=w-s'
+      [ $force -eq 0 ] && sleep $s
+    fi 
+  }
+}  
+last=$last_stamp
+fi
+
+if [ $lab -gt 0 ]; then
+  echo "wget -q -O '$mnt_out/$mnt_temp' '$mnt_url' at `date '+%F %T'`"
+fi
+if [ x"$dry" = x"" ]; then
+  wget -q -O "$mnt_out/$mnt_temp" "$mnt_url"
+  node $dir"/parse_mnt.js" $mnt_out/$mnt_temp
+  #rm -f $mnt_out"/"$mnt_temp
+fi
+fi
+)
+done
+)
+
+exit 0
---
+++ bin/parse_mnt.js
@@ -0,0 +1,151 @@
+var fs = require('fs'),
+input = __dirname.replace(/bin/,"public")+"/mnt_data/tamme/arc-file-tamme.html",
+time = new Date(),
+cd=timestr(time),
+last = 0,
+file,
+path = '',
+dir;
+
+process.argv.forEach(function (val, index, array) {
+  if(index===2 && val) {
+  	  input=val;
+  }
+});
+path = fs.realpathSync(input) || '';
+if(!path) throw "Could not resolve input file path:" + input;
+dir=path.match(/.*\//);
+
+function timestr(time) {
+	if(typeof time === 'number') time=new Date(time);
+	var month = time.getMonth()+1;
+	var day = time.getDate();
+	var datestr = time.getFullYear()+"-"+(month<10?"0":"")+month+"-"+(day<10?"0":"")+day;
+	var date = time.getFullYear()+(month<10?"0":"")+month+(day<10?"0":"")+day;
+	return [datestr,date,day];
+}
+
+function getLast(file) {
+	var last="", ret = 0;
+	if(file) {
+		try {
+		var obj = fs.readFileSync(file, 'utf-8');
+		if(obj){
+			last = obj.trim().split("\n").pop();
+			if(last) last = last.match(/^(\d+\s\d\d:\d\d)/);
+			if(last) last = last[1].replace(/(\d\d\d\d)(\d\d)(\d\d)/,"$1-$2-$3");
+			if(last) ret = new Date(last).getTime();
+		}
+		} catch (e) {
+		}
+	}
+	//console.log("got last: '" + last + "' from file: " + file )
+	return ret;
+}
+//dir = input.match(/.*\//);
+//file=dir+"ARC-"+cd[0]+".txt";
+//console.log(getLast(file));
+/*
+43 13.08.14 12:30 01.01.70 03:00
+Air temp. 19.9 C 13.08.14 12:30 #000000 #FFFFFF
+Road status kuiv 13.08.14 12:30 #C8C8C8 #FFFFFF
+Road temp. 29.3 C 13.08.14 12:30 #000000 #FFFFFF
+Precip. int. - mm/h 13.08.14 12:30 #000000 #FFFFFF
+Precip. type - 13.08.14 12:30 #269936 #FFFFFF
+Air humidity 74.0 % 13.08.14 12:30 #000000 #FFFFFF
+Dew point 15.2 C 13.08.14 12:30 #000000 #FFFFFF
+Max wind sp. 4.4 m/s 13.08.14 12:30 #000000 #FFFFFF
+Wind dir. <img src="img/arrow_10.gif" border=0> deg 13.08.14 12:30 #000000 #FFFFFF
+Wind speed 1.5 m/s 13.08.14 12:30 #000000 #FFFFFF
+Visibility 2000 m 13.08.14 12:30 #000000 #FFFFFF
+Sunrise 05:38 black white
+Sunset 21:01 black white
+*/
+
+fs.readFile(path, {encoding:'utf-8'}, function(err, data) {
+	if(err) throw err;
+	var child,
+	text = "", trs, ret=[], test, d=0, m=0, wd=[], wdata=[];
+	wd = data.split(/\n/);
+	//console.log(JSON.stringify(wd));
+	wd.forEach(function(a,b){
+		child = a.split('\t');
+		if(b===0) {
+			text = child[1].replace(/^(\d*)\.(\d*)\.(\d*) /,'20$3$2$1 ');
+			ret.push(text);
+		}
+		text = (child[1]+"").replace(/ .*$/,'').trim();
+		if(b===1) ret.push(text);
+		if(b===4) ret.push(text==='-'?0:text);
+		if(b===6) ret.push(text);
+		if(b===7) ret.push(text);
+		if(b===8) ret.push(text);
+		if(b===9) ret.push(extractWd(child[1]));
+		if(b===10) ret.push(text);
+		if(b===11) ret.push(text);
+	});
+		if (ret.length) {
+			//console.log(ret.join("\t"));
+			wdata.push(ret.join("\t"));
+		}
+	if(wdata){
+		var j = wdata.length-1, t = 0, val;
+		d = time.getTime();
+		wd = timestr(d);
+		test = false;
+		ret = {};
+		ret[wd[0]]=[];
+		last=getLast(dir+"ARC-"+wd[0]+".txt");
+		//console.log("last in input file: " + wdata[j].match(/\d+:\d+/));
+		for(; j >= 0; --j){
+			m = wdata[j].match(/^(\d*) (\d+):/)[2];
+			if(test && /^(1|2)\d/.test(m)) {
+				test=false;
+				d = d-(24*3600*1000);
+				wd = timestr(d);
+				ret[wd[0]]=[];
+				last=getLast(dir+"ARC-"+wd[0]+".txt");
+			}
+			if(/00/.test(m)) test = true;
+			t = new Date(wd[0] + " " + wdata[j].match(/^\d* (\d+:\d+)/)[1]).getTime()||0;
+			//console.log(t + " "+ last);
+			//console.log(wdata[j].split(/\s+?/));
+			if(!last||(t&&t>last)) {
+				ret[wd[0]].push(wdata[j]);
+			}
+		}
+		for(val in ret){
+			if(val && ret[val].length){
+				ret[val].reverse()
+				//console.log(ret[val]);
+				//console.log(ret[val].join("\n"));
+				file = dir+"ARC-"+val+".txt";
+				fs.appendFile(file, ret[val].join("\n")+"\n");
+			}
+		}
+	}
+});
+
+var extractWd = function(d) {
+	var s = d.match(/arrow_(\d*).gif/);
+	if(s[1]){
+		if(s[1] == 1) return 0;
+		if(s[1] == 2) return 22.5;
+		if(s[1] == 3) return 45;
+		if(s[1] == 4) return 67.5;
+		if(s[1] == 5) return 90;
+		if(s[1] == 6) return 112.5;
+		if(s[1] == 7) return 135;
+		if(s[1] == 8) return 157.5;
+		if(s[1] == 9) return 180;
+		if(s[1] == 10) return 202.5;
+		if(s[1] == 11) return 225;
+		if(s[1] == 12) return 247.5;
+		if(s[1] == 13) return 270;
+		if(s[1] == 14) return 292.5;
+		if(s[1] == 15) return 315;
+		if(s[1] == 16) return 337.5;
+	}
+	else { return 0 };
+}
+
