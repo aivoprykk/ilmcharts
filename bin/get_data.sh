a194a8f3aebb8dc55d41c3bf5a84cb86519edcd4
Aivo Prukk <aivo@majasa.ee>
2014-10-22 11:01:58 +0300

    redesign get_data

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -4,6 +4,8 @@ lab=0
 force=0
 test=0
 dry=""
+last=""
+station=""
 max=200
 dir=`dirname $0`
 echo "$dir" | grep -q '^/' || dir=`pwd`/$dir
@@ -29,20 +31,27 @@ while [ $# -ge 1 ]; do
                     dry="1"
                     shift
                     ;;
+                *jaam=*)
+                	station=${1/jaam=/};
+                	shift
+                	;;
         esac
         shift
 done
 
 rotate () {
-  ext=$1
-  dir=$2
-  name=$3
-  num=1
-  base=${name/\.${ext}/}
+  local ext=$1
+  local dir=$2
+  local n=$3
+  local num=1
+  local base=${n/\.${ext}/}
   (
   cd $dir;
-  [ -f ${name} ] || return 1;
-  [ -f ${base}.001.${ext} ] && diff $name $base.001.$ext >/dev/null && return 2;
+  local b=""
+  local c=""
+  local d=""
+  [ -f ${n} ] || return 1;
+  [ -f ${base}.001.${ext} ] && diff ${n} $base.001.$ext >/dev/null && return 2;
   ls -r *.*.${ext} 2>/dev/null|while read a; do
     b=${a/\.*/};
     c=${a/.${ext}/}
@@ -62,19 +71,15 @@ rotate () {
     cp -f $a $b.${d}.${ext};
   done
   if [ $lab -gt 0 ]; then
-    echo "cp -f ${name} $base.001.$ext"
+    echo "cp -f ${n} $base.001.$ext"
   fi
-  cp -f ${name} $base.001.$ext;
+  cp -f ${n} $base.001.$ext;
   )
   return 0;
 }
 
-yr_time () {
 #<nextupdate>2013-05-13T19:00:00</nextupdate>
-dir=$1
-file=$2
-grep nextupdate ${dir}/${file} | perl -MTime::Local -ne '
-$ltime=$time=time;
+yrfilter='$ltime=$time=time;
 if(/<nextupdate>/) {
 s/^.*<nextupdate>([^\<]+)<.*$/$1/;
 if(/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/){
@@ -83,17 +88,11 @@ $ltime=timelocal($sec,$min,$hour,$mday,$month-1,$year-1900);
 print $ltime - $time;
 exit 0;
 }
-}
 print 0;
-'
-}
-
-wg_time () {
+exit 0;
+}'
 #Mon, 13 May 2013 16:55:00 +0000
-dir=$1
-file=$2
-grep update_next ${dir}/${file} | perl -MTime::Local -ne '
-$gmtime=$time=time;
+wgfilter='$gmtime=$time=time;
 %months=("Jan"=>1,"Feb"=>2,"Mar"=>3,"Apr"=>4,"May"=>5,"Jun"=>6,"Jul"=>7,"Aug"=>8,"Sep"=>9,"Oct"=>10,"Nov"=>11,"Dec"=>12);
 if(/update_next/) {
 s/^.*update_next":"([^\"]+)".*$/$1/;
@@ -103,9 +102,20 @@ $gmtime=timegm($sec,$min,$hour,$mday,$month-1,$year-1900);
 print $gmtime - $time;
 exit 0;
 }
-}
 print 0;
-'
+exit 0;
+}'
+
+xtime () {
+	local dir=$1
+	local file=$2
+	local p=$3
+	local nail=$p"nail"
+	local filter=$p"filter"
+	local time=-1;
+	[ -f ${dir}/${file} ] && time=$(cat ${dir}/${file} | perl -MTime::Local -ne "${!filter}")
+	[ x"$time" = x"" ] && time=-1
+	echo $time
 }
 
 (
@@ -121,98 +131,68 @@ IFS=:
 set $j
 place=$1
 title=$2
-wg=$3
-yr=$4
-emhi=$5
-mnt=$6
+names="wg:$3 yr:$4 emhi:$5 mnt:$6 zoig:$7 emu:$8";
 IFS=' '
 
 (
 cd public
 #echo "data: "$place $title $wg $yr;
 [ $test -eq 0 ] || continue;
-if [ x"$wg" != x"" ]; then
-	
-#wguru
-wg_file=windguru_forecast.json
-wg_dir=wg_data/$place
-wg_place=$wg
-#[ "$place" = "tamme" ] && wg_place=192609
-wg_url='http://www.windguru.cz/int/widget_json.php?callback=wg_data&lng=ee&s='$wg_place'&params=[WINDSPD,GUST,MWINDSPD,SMER,TMPE,TCDC,APCPs]&odh=0&doh=24&fhours=72&wj=msd&tj=c&waj=m&lng=ee'
-[ -d "${wg_dir}" ] || {  [ x"$wg_dir" != x"" ] && mkdir -p "${wg_dir}" || continue; }
-if [ -e ${wg_dir}/${wg_file} ]; then
-  wg_t=`wg_time ${wg_dir} ${wg_file}`;
-else
-  wg_t=-1
-fi
-if [ $lab -gt 0 ]; then
-  echo "windguru secs "$wg_t
-fi
-if [ $force -gt 0 -o $wg_t -lt 0 ]; then
+[ x"$place" = x"" ] && continue;
+for x in $names; do
+name=${x/:*/};
+value=${x/*:/};
 
-  rotate json ${wg_dir} ${wg_file}
+#echo "$place $name $value";
+[ x"$value" = x"" ] && continue;
+#continue;
+[ x"$station" != x"" -a "$place" != "$station" ] && continue;
+#[ x$place = xparnu ] || continue;
+case "$name" in
+wg)
+	url='http://www.windguru.cz/int/widget_json.php?callback=wg_data&lng=ee&s='$value'&params=[WINDSPD,GUST,MWINDSPD,SMER,TMPE,TCDC,APCPs]&odh=0&doh=24&fhours=72&wj=msd&tj=c&waj=m&lng=ee'
+	out=wg_data/$place
+	file=windguru_forecast.json
+	;;
+yr)
+	url='http://www.yr.no/sted/Estland/'$value'/varsel_time_for_time.xml'
+	out=yr_data/$place
+	file=forecast_hour_by_hour.xml
+	;;
+*)
+	continue;
+	;;
+esac
+
+#echo "$name $value $out"
+[ -d "${out}" ] || {  [ x"$out" != x"" ] && mkdir -p "${out}" || continue; }
+t=`xtime ${out} ${file} ${name}`;
+if [ $lab -gt 0 ]; then
+  echo "$name secs "$t
+fi
+
+#continue
+
+if [ $force -gt 0 -o $t -lt 0 ]; then
+	[ $name = "wg" ] && rotate json ${out} ${file}
+	[ $name = "yr" ] && rotate xml ${out} ${file}
   if [ $lab -gt 0 ]; then
-    echo wget -q -O ${wg_dir}/${wg_file}.tmp "${wg_url}"
+    echo wget -q -O ${out}/${file}.tmp "${url}"
   fi
   if [ x"$dry" = x"" ]; then
-    wget -q -O ${wg_dir}/${wg_file}.tmp "${wg_url}"
+    wget -q -O ${out}/${file}.tmp "${url}"
   fi
+  t=`xtime ${out} ${file}.tmp ${name}`
   if [ $lab -gt 0 ]; then
-    printf "next update time: "
-    wg_time ${wg_dir} ${wg_file}.tmp
-    echo
+    echo "next update time: $t"
   fi
-  if [ -f ${wg_dir}/${wg_file}.tmp ]; then
-    if [ `wg_time ${wg_dir} ${wg_file}.tmp` -gt 0 ]; then
-      cp -f ${wg_dir}/${wg_file}.tmp ${wg_dir}/${wg_file}
-      rm -f ${wg_dir}/${wg_file}.tmp
-    fi
-  fi
-
+	if [ $t -gt 0 ]; then
+	  cp -f ${out}/${file}.tmp ${out}/${file}
+	  rm -f ${out}/${file}.tmp
+	fi
 fi
-fi
-
-#yr.no
-if [ x"$yr" != x"" ]; then
-yr_file=forecast_hour_by_hour.xml
-yr_dir=yr_data/$place
-yr_place=$yr;
-#[ "$place" = "tamme" ] && yr_place='Tartumaa/Tamme'
-yr_url='http://www.yr.no/sted/Estland/'$yr_place'/varsel_time_for_time.xml'
-[ -d "${yr_dir}" ] || { [ x"${yr_dir}" != x"" ] && mkdir -p "${yr_dir}" || continue; }
-
-if [ -e ${yr_dir}/${yr_file} ]; then
-  yr_t=`yr_time ${yr_dir} ${yr_file}`;
-else
-  yr_t=-1
-fi
-
-if [ $lab -gt 0 ]; then
-  echo "yr.no secs "$yr_t
-fi
-if [ $force -gt 0 -o $yr_t -lt 0 ]; then
-
-  rotate xml ${yr_dir} ${yr_file}
-  if [ $lab -gt 0 ]; then
-    echo wget -q -O ${yr_dir}/${yr_file}.tmp "${yr_url}"
-  fi
-  if [ x"$dry" = x"" ]; then
-    wget -q -O ${yr_dir}/${yr_file}.tmp "${yr_url}"
-  fi
-  if [ $lab -gt 0 ]; then
-    printf "next update time: "
-    yr_time ${yr_dir} ${yr_file}.tmp
-    echo
-  fi
-  if [ -f ${yr_dir}/${yr_file}.tmp ]; then
-    if [ `yr_time ${yr_dir} ${yr_file}.tmp` -gt 0 ]; then
-      cp -f ${yr_dir}/${yr_file}.tmp ${yr_dir}/${yr_file}
-      rm -f ${yr_dir}/${yr_file}.tmp
-    fi
-  fi
-fi
-fi
+done ##split conf
 )
-#done ##split conf
+
 done ##places
 )
