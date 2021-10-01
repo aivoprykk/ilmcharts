474497fc138598b549c356bba54248d2c1bf8249
Aivo Prükk <aivo@piibel.net>
2021-10-01 21:14:19 +0300

    v1.2.1 fix bin/get_data

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -129,10 +129,10 @@ if($time0=~/^\w+, (\d{2}) (\w+) (\d{4}) (\d{2}):(\d{2}):(\d{2})/){
 $year=$3;$month=$months{$2};$mday=$1;$hour=$4;$min=$5;$sec=$6;
 $time=timegm($sec,$min,$hour,$mday,$month-1,$year-1900);
 }
-if(/^(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2}):(\d{2})/){
+if(/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})/){
 $year=$1;$month=$2;$mday=$3;$hour=$4;$min=$5;$sec=$6;
 $gmtime=timegm($sec,$min,$hour+1,$mday,$month-1,$year-1900);
-print $time - $gmtime;
+print ($gmtime - $time);
 exit 0;
 }
 print 0;
@@ -162,7 +162,8 @@ xtime () {
 	local filter=$p"filter"
 	local time=-1;
   local time0="$4"
-	[ -f ${dir}/${file} ] && time=$(cat ${dir}/${file} | perl -MTime::Local -ne "\$time0='$time0';${!filter}")
+  [ ! -f ${dir}/${file} ] && echo 0 && return 0;
+	time=$(cat ${dir}/${file} | perl -MTime::Local -ne "\$time0=\"${time0}\";${!filter}")
 	[ x"$time" = x"" ] && time=-1
 	echo $time
 }
@@ -236,11 +237,11 @@ fi
 
 #continue
 
-if [ $force -gt 0 -o $t -lt 0 ]; then
+if [ $force -gt 0 -o $t -le 0 ]; then
   #[ $name = "wg" ] && rotate json ${out} ${file}
   #[ $name = "yr" ] && rotate xml ${out} ${file}
   if [ $lab -gt 0 -o x$dry != x ]; then
-    echo wget -U "$meinfo" -q -O ${out}/${file}.tmp "${url}"
+    echo wget -T10 --tries=1 -U "$meinfo" -q -O ${out}/${file}.tmp "${url}"
   fi
   if [ x"$dry" = x"" ]; then
     wget -T10 --tries=1 -U "$meinfo" -q -O ${out}/${file}.tmp "${url}"
@@ -248,23 +249,41 @@ if [ $force -gt 0 -o $t -lt 0 ]; then
       wget -T10 --tries=1 -U "$meinfo" -q -O ${out}/${file2}.tmp "${url2}"
     fi
   fi
-  t=`xtime ${out} ${file}.tmp ${name}`
+  t=`xtime ${out} ${file}.tmp ${name} "${t0}"`
   if [ $lab -gt 0 ]; then
     echo "next update time: $t"
   fi
-  if [ x$dry = x ]; then
-    if [ -e "${out}/${file}" -a $t -lt 0 ]; then
-      mv "${out}/${file}" "${out}/${file}.${now}"
-    fi
-    mv -f "${out}/${file}.tmp" "${out}/${file}"
-    if [ x"$file2" != x"" ]; then
-      if [ -e "${out}/${file2}"  -a $t -lt 0 ]; then
-        mv "${out}/${file2}" "${out}/${file2}.${now}"
+  if [ -e "${out}/${file}.tmp" ]; then
+    if [ -e "${out}/${file}" -a $t -gt 0 ]; then
+      if [ x$dry = x ]; then
+          mv "${out}/${file}" "${out}/${file}.${now}"
+      else
+          echo mv "${out}/${file}" "${out}/${file}.${now}"
       fi
-      mv -f "${out}/${file2}.tmp" "${out}/${file2}"
     fi
-    #rm -f ${out}/${file}.tmp
+    if [ x$dry = x ]; then
+      mv -f "${out}/${file}.tmp" "${out}/${file}"
+    else
+      echo mv -f "${out}/${file}.tmp" "${out}/${file}"
+    fi
   fi
+  if [ x"$file2" != x"" ]; then
+    if [ -e "${out}/${file2}.tmp" ]; then
+      if [ -e "${out}/${file2}"  -a $t -gt 0 ]; then
+        if [ x$dry = x ]; then
+          mv "${out}/${file2}" "${out}/${file2}.${now}"
+        else
+          echo mv "${out}/${file2}" "${out}/${file2}.${now}"
+        fi
+      fi
+      if [ x$dry = x ]; then
+        mv -f "${out}/${file2}.tmp" "${out}/${file2}"
+      else
+        echo mv -f "${out}/${file2}.tmp" "${out}/${file2}"
+      fi
+    fi
+  fi
+  #rm -f ${out}/${file}.tmp
 fi
 
 done ##split conf
